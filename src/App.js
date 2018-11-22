9cd98cfecce2562e58b4b9d54047f98a8b25ebc9
ywhan <ywhan@ibizsoftware.net>
2018-11-22 10:45:21 +0900

    craete react app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { __await } from 'tslib';
 
 class App extends Component {
+
+  state = {
+    isLoading : true,
+    groups : []
+  }
+
+  /**
+   * 컴포넌트를 생성 할 때는 constructor -> componentWillMount -> render -> componentDidMount 순으로 진행
+   * state의 값을 세팅하기 위한 함수
+   */
+  async componentDidMount() {
+    const response = await fetch('/api/groups');
+    const body = await response.json();
+    this.setState({groups:body, isLoading : false});
+  }
+
   render() {
     return (
       <div className="App">
